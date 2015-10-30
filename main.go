eb32d945c172874702034c472b4fb7d408f2e6b3
leaf corcoran <leafot@gmail.com>
2015-10-29 17:37:13 -0700

    add a version

--- main.go
+++ main.go
@@ -12,12 +12,16 @@ var (
 	configFname string
 )
 
+const version = "1.0"
+
 func init() {
 	flag.StringVar(&configFname, "config",
 		dullcache.DefaultConfigFname, "Path to json config")
 
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: dullcache [OPTIONS]\n\nOptions:\n")
+		fmt.Fprintf(os.Stderr, "dullcache version %v\n", version)
+		fmt.Fprintf(os.Stderr, "Usage: dullcache [OPTIONS]\n")
+		fmt.Fprintf(os.Stderr, "\nOptions:\n")
 		flag.PrintDefaults()
 	}
 }
