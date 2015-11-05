9c1b022ce8e778f286951cf31c699d9974c6ff52
leaf corcoran <leafot@gmail.com>
2015-11-05 10:19:12 -0800

    add some comments

--- dullcache/filecache.go
+++ dullcache/filecache.go
@@ -33,24 +33,29 @@ func NewFileCache(basePath string) *FileCache {
 	}
 }
 
+// Returns the total number of paths the file cache is tracking
 func (cache *FileCache) CountAvailablePaths() int {
 	cache.availableMutex.RLock()
 	defer cache.availableMutex.RUnlock()
 	return len(cache.availablePaths)
 }
 
+// Returns the total number of paths that are currently marked as busy
 func (cache *FileCache) CountBusyPaths() int {
 	cache.busyMutex.RLock()
 	defer cache.busyMutex.RUnlock()
 	return len(cache.busyPaths)
 }
 
+// Returns the total number of paths that are currently marked as purged
 func (cache *FileCache) CountPurgedPaths() int {
 	cache.purgedMutex.RLock()
 	defer cache.purgedMutex.RUnlock()
 	return len(cache.purgedPaths)
 }
 
+// Takes a subpath from the original request and converts it to a path on the
+// filesystem where the cache should store it's copy of the file
 func (cache *FileCache) CacheFilePath(subPath string) (string, error) {
 	fname := b58.Encode([]byte(subPath))
 
@@ -61,12 +66,15 @@ func (cache *FileCache) CacheFilePath(subPath string) (string, error) {
 	return path.Join(cache.basePath, fname), nil
 }
 
+// Checks if a path is available for being served to client
 func (cache *FileCache) PathAvailable(path string) http.Header {
 	cache.availableMutex.RLock()
 	defer cache.availableMutex.RUnlock()
 	return cache.availablePaths[path]
 }
 
+// Check if we have the path on the filesystem but not tracked. This is used to
+// keep re-use cache files across server restarts
 func (cache *FileCache) PathMaybeAvailable(path string) (int64, error) {
 	path, err := cache.CacheFilePath(path)
 
@@ -87,18 +95,25 @@ func (cache *FileCache) PathMaybeAvailable(path string) (int64, error) {
 	return info.Size(), nil
 }
 
+// Checks if a path is currently busy
 func (cache *FileCache) PathBusy(path string) bool {
 	cache.busyMutex.RLock()
 	defer cache.busyMutex.RUnlock()
 	return cache.busyPaths[path]
 }
 
+// Mark a path as being available to be served by the cache, takes the headers
+// from the backend request to fetch the file
 func (cache *FileCache) MarkPathAvailable(path string, headers http.Header) {
 	cache.availableMutex.Lock()
 	defer cache.availableMutex.Unlock()
 	cache.availablePaths[path] = headers
 }
 
+// Marks a path as busy. Paths should be marked busy when any write disk
+// operation are happening so no new requests try to consume the file
+//
+// Returns true if it was able to mark the bath busy, false otherwise
 func (cache *FileCache) MarkPathBusy(path string) bool {
 	cache.busyMutex.Lock()
 	defer cache.busyMutex.Unlock()
@@ -111,18 +126,22 @@ func (cache *FileCache) MarkPathBusy(path string) bool {
 	return true
 }
 
+// Mark a path as no longer busy
 func (cache *FileCache) MarkPathFree(path string) {
 	cache.busyMutex.Lock()
 	defer cache.busyMutex.Unlock()
 	delete(cache.busyPaths, path)
 }
 
+// Check is a path needs a purge
 func (cache *FileCache) PathNeedsPurge(path string) bool {
 	cache.purgedMutex.RLock()
 	defer cache.purgedMutex.RUnlock()
 	return cache.purgedPaths[path]
 }
 
+// Remove the purge mark for a path. Typically called after the purge has been
+// fulfilled. Returns true if the purge status was removed
 func (cache *FileCache) ReleasePathPurge(path string) bool {
 	cache.purgedMutex.RLock()
 	needsPurge := cache.purgedPaths[path]
@@ -140,13 +159,16 @@ func (cache *FileCache) ReleasePathPurge(path string) bool {
 	return false
 }
 
+// Mark a path that it needs a purge. A purged file will redownload and cache
+// from the backend on the next request.
 func (cache *FileCache) MarkPathNeedsPurge(path string) {
 	cache.purgedMutex.Lock()
 	defer cache.purgedMutex.Unlock()
 	cache.purgedPaths[path] = true
 }
 
-// size in bytes of all the available paths
+// Count the entire size of tracked files in bytes from the stored
+// Content-Length headers
 func (cache *FileCache) TrackedSize() int64 {
 	cache.availableMutex.RLock()
 	defer cache.availableMutex.RUnlock()
@@ -167,6 +189,7 @@ func (cache *FileCache) TrackedSize() int64 {
 	return total
 }
 
+// Remove a path from the cache
 func (cache *FileCache) DeletePath(path string) error {
 	fname, err := fileCache.CacheFilePath(path)
 
